ad75e5bd606da46dab3ed59479eaa6dbb03cb268
jnphilipp <me@jnphilipp.org>
2016-07-09 16:19:21 +0200

    Add Journal model to journals app and delete unnecessary files.

--- bibliothek/journals/apps.py
+++ bibliothek/journals/apps.py
@@ -1,5 +1,9 @@
+# -*- coding: utf-8 -*-
+
 from django.apps import AppConfig
 
 
 class JournalsConfig(AppConfig):
     name = 'journals'
+    verbose_name = 'journal'
+    verbose_name_plural = 'journals'
--- bibliothek/journals/models.py
+++ bibliothek/journals/models.py
@@ -1,3 +1,41 @@
-from django.db import models
+# -*- coding: utf-8 -*-
 
-# Create your models here.
+from django.db import models
+from django.template.defaultfilters import slugify
+from links.models import Link
+
+
+class TextFieldSingleLine(models.TextField):
+    pass
+
+
+class Journal(models.Model):
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    slug = models.SlugField(max_length=2048, unique=True)
+    name = TextFieldSingleLine(unique=True)
+    links = models.ManyToManyField(Link, related_name='journals', blank=True)
+
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        else:
+            orig = Journal.objects.get(pk=self.id)
+            if orig.name != self.name:
+                self.slug = slugify(self.name)
+        super(Journal, self).save(*args, **kwargs)
+
+
+    def to_json(self):
+        return {'name': self.name}
+
+
+    def __str__(self):
+        return self.name
+
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = ' journal'
