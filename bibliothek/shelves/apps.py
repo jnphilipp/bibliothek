fb059520b987d8de1840722a4ff9fbd6a98cae9c
jnphilipp <me@jnphilipp.org>
2016-07-09 19:44:59 +0200

    Add Read model to shelves app and delete unnecessary files.

--- bibliothek/shelves/apps.py
+++ bibliothek/shelves/apps.py
@@ -1,5 +1,9 @@
+# -*- coding: utf-8 -*-
+
 from django.apps import AppConfig
 
 
 class ShelvesConfig(AppConfig):
     name = 'shelves'
+    verbose_name = 'shelf'
+    verbose_name_plural = 'shelves'
--- bibliothek/shelves/models.py
+++ bibliothek/shelves/models.py
@@ -1,3 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
-# Create your models here.
+
+class Read(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    started = models.DateField(verbose_name=' date started', blank=True, null=True)
+    finished = models.DateField(verbose_name=' date finished', blank=True, null=True)
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+
+    def __str__(self):
+        return 'read "%s"' % self.content_object
+
+
+    class Meta:
+        ordering = ('started', 'finished')
+        verbose_name = ' read'
