13309ee7d24a9c8ca8fbc827c5ec3e6a00e109cd
jnphilipp <me@jnphilipp.org>
2016-07-12 11:53:39 +0200

    Add function to create paper from bibtex.

--- bibliothek/papers/functions/bibtex.py
+++ bibliothek/papers/functions/bibtex.py
@@ -13,33 +13,33 @@ def parse(bibtex_file):
 
     entries = []
     for entry in bib_database.entries:
-        title = entry['title'] if 'title' in entry else ''
+        title = entry['title'].strip() if 'title' in entry else ''
 
         authors = []
         for author in re.compile(r'\s+and\s+').split(re.sub(r'(?s)\s*\n\s*', ' ', entry['author'])):
             if ',' in author:
                 s = author.split(',')
-                authors.append({'first_name':s[1], 'last_name':s[0]})
+                authors.append({'first_name':s[1].strip(), 'last_name':s[0].strip()})
             else:
                 s = author.rsplit(' ', 1)
-                authors.append({'first_name':s[0], 'last_name':s[1]})
+                authors.append({'first_name':s[0].strip(), 'last_name':s[1].strip()})
 
-        journal = entry['journal'] if 'journal' in entry else ''
-        volume = entry['volume'] if 'volume' in entry else ''
+        journal = entry['journal'].strip() if 'journal' in entry else ''
+        volume = entry['volume'].strip() if 'volume' in entry else ''
         volume = '%s.%s' % (volume, entry['number']) if 'number' in entry else volume
         if 'eprint' in entry and not volume:
-            volume = entry['eprint']
-        publisher = entry['publisher'] if 'publisher' in entry else ''
+            volume = entry['eprint'].strip()
+        publisher = entry['publisher'].strip() if 'publisher' in entry else ''
 
-        year = int(entry['year']) if 'year' in entry else None
-        month = entry['month'] if 'month' in entry else None
+        year = int(entry['year'].strip()) if 'year' in entry else None
+        month = entry['month'].strip() if 'month' in entry else None
         if year and month:
             date = datetime.strptime('%s %s' % (month, year), '%b %Y')
         elif year:
             date = datetime(year, 1, 1)
 
-        published_on = datetime.strptime(entry['timestamp'], '%a, %d %b %Y %H:%M:%S %z').date() if 'timestamp' in entry else date.date()
-        url = entry['link'] if 'link' in entry else ''
+        published_on = datetime.strptime(entry['timestamp'].strip(), '%a, %d %b %Y %H:%M:%S %z').date() if 'timestamp' in entry else date.date()
+        url = entry['link'].strip() if 'link' in entry else ''
 
         entries.append({'title':title, 'authors':authors, 'journal':journal, 'volume':volume, 'publisher':publisher, 'published_on':published_on, 'url':url, 'bibtex':bibtex})
     return entries
---
+++ bibliothek/papers/functions/paper/create.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from datetime import date
+from django.core.files import File as DJFile
+from files.models import File
+from journals.models import Journal
+from links.models import Link
+from papers.models import Paper
+from persons.models import Person
+from shelves.models import Acquisition
+from utils import lookahead, stdout
+
+from .. import bibtex
+
+
+def from_bibtex(bibtex_file, files=[]):
+    papers = bibtex.parse(bibtex_file)
+    if len(papers) != len(files):
+        for i in range(len(files), len(papers)):
+            files.append(None)
+    for paper, file in zip(papers, files):
+        from_dict(paper, [file, bibtex_file] if file else [bibtex_file])
+        print()
+
+
+def from_dict(paper_dict, files=[]):
+    positions=[.33, 1.]
+    print('=' * 100)
+
+    if paper_dict['title']:
+        stdout.p(['Title:', paper_dict['title']], positions=positions)
+        paper, created = Paper.objects.get_or_create(title=paper_dict['title'])
+        if not created:
+            stdout.p(['The paper already exists, aborting...'], after='=')
+            return
+        else:
+            paper.bibtex = paper_dict['bibtex']
+    else:
+        stdout.p(['No title, aborting...'], after='=')
+        return
+
+    for (i, author), has_next in lookahead(enumerate(paper_dict['authors'])):
+        if i == 0:
+            stdout.p(['Authors:', author], positions=positions, after=None if has_next else '_')
+        else:
+            stdout.p(['', author], positions=positions, after=None if has_next else '_')
+        person, created = Person.objects.get_or_create(first_name=author['first_name'], last_name=author['last_name'])
+        paper.authors.add(person)
+
+    if paper_dict['journal']:
+        stdout.p(['Journal:', paper_dict['journal']], positions=positions)
+        journal, created = Journal.objects.get_or_create(name=paper_dict['journal'])
+        paper.journal = journal
+
+    if paper_dict['volume']:
+        stdout.p(['Volume:', paper_dict['volume']], positions=positions)
+        paper.volume = paper_dict['volume']
+
+    if paper_dict['published_on']:
+        stdout.p(['Published on:', paper_dict['published_on']], positions=positions)
+        paper.published_on = paper_dict['published_on']
+
+    if paper_dict['url']:
+        stdout.p(['URL:', paper_dict['url']], positions=positions)
+        link, created = Link.objects.get_or_create(link=paper_dict['url'])
+        paper.links.add(link)
+
+    for (i, file), has_next in lookahead(enumerate(files)):
+        stdout.p(['File:' if i == 0 else '', file], positions=positions, after=None if has_next else '_')
+        file_name = os.path.basename(file)
+        file_obj = File()
+        file_obj.file.save(file_name, DJFile(open(file, 'rb')))
+        file_obj.content_object = paper
+        file_obj.save()
+    paper.save()
+    stdout.p(['Successfully added paper "%s" with id "%s".' % (paper.title, paper.id)], positions=[1.])
+    
+    acquisition = Acquisition.objects.create(date=date.today(), content_object=paper)
+    stdout.p(['Successfully added acquisition on "%s".' % acquisition.date], after='=', positions=[1.])
