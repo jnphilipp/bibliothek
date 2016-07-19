65143aadb999ae9f752d3539a007bb068ecacdd5
jnphilipp <me@jnphilipp.org>
2016-07-19 22:15:01 +0200

    Add admin and update views and templates in journals app.

--- bibliothek/journals/views.py
+++ bibliothek/journals/views.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
 
+from django.db.models import Count
 from django.shortcuts import get_object_or_404, render
 from journals.models import Journal
 
 
 def journals(request):
-    journals = Journal.objects.all()
+    o = request.GET.get('o') if request.GET.get('o') else 'name'
+    journals = Journal.objects.annotate(cp=Count('papers')).all().order_by(o)
     return render(request, 'journals/journal/journals.html', locals())
 
 
 def journal(request, slug):
+    o = request.GET.get('o') if request.GET.get('o') else 'title'
     journal = get_object_or_404(Journal, slug=slug)
+    papers = journal.papers.all().order_by(o)
     return render(request, 'journals/journal/journal.html', locals())
