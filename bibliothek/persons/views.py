95b802bf55cb59d0f93ab7579003f16e639f68f8
jnphilipp <me@jnphilipp.org>
2016-07-19 22:14:23 +0200

    Update persons views and templates.

--- bibliothek/persons/views.py
+++ bibliothek/persons/views.py
@@ -1,14 +1,22 @@
 # -*- coding: utf-8 -*-
 
+from django.db.models import Count
 from django.shortcuts import get_object_or_404, render
 from persons.models import Person
 
 
 def persons(request):
-    persons = Person.objects.all()
+    o = request.GET.get('o') if request.GET.get('o') else 'name'
+    persons = Person.objects.annotate(cp=Count('papers')).all()
+    if o.endswith('name'):
+        persons = persons.order_by('last_name', 'first_name')
+    else:
+        persons = persons.order_by(o)
     return render(request, 'persons/person/persons.html', locals())
 
 
 def person(request, slug):
+    o = request.GET.get('o') if request.GET.get('o') else 'title'
     person = get_object_or_404(Person, slug=slug)
+    papers = person.papers.all().order_by(o)
     return render(request, 'persons/person/person.html', locals())
