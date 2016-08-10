35eecbe84662225839d62cee0bbc3b127c558e30
jnphilipp <me@jnphilipp.org>
2016-08-10 15:43:17 +0200

    Update paper create module.

--- bibliothek/papers/functions/paper/create.py
+++ bibliothek/papers/functions/paper/create.py
@@ -30,7 +30,7 @@ def from_dict(paper_dict, files=[]):
     print('=' * 100)
 
     if paper_dict['title']:
-        stdout.p(['Title:', paper_dict['title']], positions=positions)
+        stdout.p(['Title', paper_dict['title']], positions=positions)
         paper, created = Paper.objects.get_or_create(title=paper_dict['title'])
         if not created:
             stdout.p(['The paper already exists, aborting...'], after='=')
@@ -43,32 +43,32 @@ def from_dict(paper_dict, files=[]):
 
     for (i, author), has_next in lookahead(enumerate(paper_dict['authors'])):
         if i == 0:
-            stdout.p(['Authors:', author], positions=positions, after=None if has_next else '_')
+            stdout.p(['Authors', author], after=None if has_next else '_', positions=positions)
         else:
-            stdout.p(['', author], positions=positions, after=None if has_next else '_')
+            stdout.p(['', author], after=None if has_next else '_', positions=positions)
         person, created = Person.objects.get_or_create(first_name=author['first_name'], last_name=author['last_name'])
         paper.authors.add(person)
 
     if paper_dict['journal']:
-        stdout.p(['Journal:', paper_dict['journal']], positions=positions)
+        stdout.p(['Journal', paper_dict['journal']], positions=positions)
         journal, created = Journal.objects.get_or_create(name=paper_dict['journal'])
         paper.journal = journal
 
     if paper_dict['volume']:
-        stdout.p(['Volume:', paper_dict['volume']], positions=positions)
+        stdout.p(['Volume', paper_dict['volume']], positions=positions)
         paper.volume = paper_dict['volume']
 
     if paper_dict['published_on']:
-        stdout.p(['Published on:', paper_dict['published_on']], positions=positions)
+        stdout.p(['Published on', paper_dict['published_on']], positions=positions)
         paper.published_on = paper_dict['published_on']
 
     if paper_dict['url']:
-        stdout.p(['URL:', paper_dict['url']], positions=positions)
+        stdout.p(['Links', paper_dict['url']], positions=positions)
         link, created = Link.objects.get_or_create(link=paper_dict['url'])
         paper.links.add(link)
 
     for (i, file), has_next in lookahead(enumerate(files)):
-        stdout.p(['File:' if i == 0 else '', file], positions=positions, after=None if has_next else '_')
+        stdout.p(['Files' if i == 0 else '', file], after=None if has_next else '_', positions=positions)
         file_name = os.path.basename(file)
         file_obj = File()
         file_obj.file.save(file_name, DJFile(open(file, 'rb')))
