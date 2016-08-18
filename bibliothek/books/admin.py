405bd6258d296ec3d71bc64f7fefa7c417c916bb
jnphilipp <me@jnphilipp.org>
2016-08-18 23:41:12 +0200

    Update books admin module.

--- bibliothek/books/admin.py
+++ bibliothek/books/admin.py
@@ -1,5 +1,35 @@
 # -*- coding: utf-8 -*-
 
+from books.models import Book, TextFieldSingleLine
 from django.contrib import admin
+from django.db import models
+from django.forms import Textarea, TextInput
+from django.utils.translation import ugettext as _
 
-# Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    def get_authors(self, inst):
+        return ', '.join([str(a) for a in inst.authors.all()])
+
+    list_display = ('title', 'get_authors', 'series', 'volume')
+    list_filter = ('authors', 'series')
+    readonly_fields = ('slug',)
+    search_fields = ('title', 'authors__first_name', 'authors__last_name', 'series__name', 'volume')
+
+    get_authors.admin_order_field = 'authors'
+    get_authors.short_description = _('Authors')
+
+    formfield_overrides = {
+        TextFieldSingleLine: {'widget': TextInput(attrs={'autocomplete':'off', 'style':'min-width:50%;'})},
+    }
+
+    fieldsets = [
+        (None, {'fields': ['slug', 'title', 'authors']}),
+        ('Series', {'fields': ['series', 'volume']}),
+        ('Links', {'fields': ['links']}),
+    ]
+
+    filter_horizontal = ('authors', 'links')
+
+
+admin.site.register(Book, BookAdmin)
