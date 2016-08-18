781837baf83556772274287e314b3682b8e2c15f
jnphilipp <me@jnphilipp.org>
2016-08-18 22:12:31 +0200

    Add Series model to series app.

--- bibliothek/series/models.py
+++ bibliothek/series/models.py
@@ -1,5 +1,36 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+
+class TextFieldSingleLine(models.TextField):
+    pass
+
+
+class Series(models.Model):
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    slug = models.SlugField(max_length=2048, unique=True)
+    name = TextFieldSingleLine(unique=True)
+
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        else:
+            orig = Series.objects.get(pk=self.id)
+            if orig.name != self.name:
+                self.slug = slugify(self.name)
+        super(Series, self).save(*args, **kwargs)
+
+
+    def __str__(self):
+        return self.name
+
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Series'
+        verbose_name_plural = 'Series'
