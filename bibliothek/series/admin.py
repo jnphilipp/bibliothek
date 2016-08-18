c77328f3a358be4d887d3869fbcb42767e12a4ac
jnphilipp <me@jnphilipp.org>
2016-08-18 22:13:50 +0200

    Update series admin module.

--- bibliothek/series/admin.py
+++ bibliothek/series/admin.py
@@ -1,5 +1,22 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib import admin
+from django.forms import TextInput
+from series.models import Series, TextFieldSingleLine
 
-# Register your models here.
+
+class SeriesAdmin(admin.ModelAdmin):
+    list_display = ('name', 'updated_at')
+    readonly_fields = ('slug',)
+    search_fields = ('name',)
+
+    formfield_overrides = {
+        TextFieldSingleLine: {'widget': TextInput(attrs={'autocomplete':'off'})},
+    }
+
+    fieldsets = [
+        (None, {'fields': ['slug', 'name']}),
+    ]
+
+
+admin.site.register(Series, SeriesAdmin)
