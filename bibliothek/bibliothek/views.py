8b568b9c0280574f2a7ca4a08eae28e2e84d20b6
jnphilipp <me@jnphilipp.org>
2016-09-08 15:08:48 +0200

    Update dashboard view and template.

--- bibliothek/bibliothek/views.py
+++ bibliothek/bibliothek/views.py
@@ -8,7 +8,7 @@ from papers.models import Paper
 
 def dashboard(request):
     bo = request.GET.get('bo') if request.GET.get('bo') else 'title'
-    books = Book.objects.order_by(bo)
+    books = Book.objects.filter(editions__reads__started__isnull=False).filter(editions__reads__finished__isnull=True).order_by(bo)
 
     mo = request.GET.get('mo') if request.GET.get('mo') else 'magazine'
     issues = Issue.objects.filter(reads__started__isnull=False).filter(reads__finished__isnull=True).order_by(mo)
