0a94da4c0599a755c2f25a3a8f72169b1ff900f8
jnphilipp <mail@jnphilipp.org>
2017-11-18 11:38:08 +0100

    Update links app.

--- bibliothek/links/admin.py
+++ bibliothek/links/admin.py
@@ -1,11 +1,26 @@
 # -*- coding: utf-8 -*-
+# Copyright (C) 2016-2017 Nathanael Philipp (jnphilipp) <mail@jnphilipp.org>
+#
+# This file is part of bibliothek.
+#
+# bibliothek is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# bibliothek is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with bibliothek.  If not, see <http://www.gnu.org/licenses/>.
 
 from django.contrib import admin
-from django.db.models import Count
-from django.forms import TextInput
 from links.models import Link
 
 
+@admin.register(Link)
 class LinkAdmin(admin.ModelAdmin):
     list_display = ('link', 'updated_at')
     search_fields = ('link',)
@@ -14,6 +29,3 @@ class LinkAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['link']}),
     ]
-
-
-admin.site.register(Link, LinkAdmin)
--- bibliothek/links/models.py
+++ bibliothek/links/models.py
@@ -1,14 +1,40 @@
 # -*- coding: utf-8 -*-
+# Copyright (C) 2016-2017 Nathanael Philipp (jnphilipp) <mail@jnphilipp.org>
+#
+# This file is part of bibliothek.
+#
+# bibliothek is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# bibliothek is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with bibliothek.  If not, see <http://www.gnu.org/licenses/>.
 
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 
 class Link(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Created at'))
-    updated_at = models.DateTimeField(auto_now=True, verbose_name=_('Updated at'))
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_('Created at')
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name=_('Updated at')
+    )
 
-    link = models.URLField(max_length=4096, unique=True, verbose_name=_('Link'))
+    link = models.URLField(
+        max_length=4096,
+        unique=True,
+        verbose_name=_('Link')
+    )
 
     def to_json(self):
         return {'link': self.link}
