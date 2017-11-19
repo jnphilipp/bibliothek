dda0c0b84df38b82e2530df0818bbb78c3fe8f21
jnphilipp <mail@jnphilipp.org>
2017-11-19 17:20:27 +0100

    Update files app.

--- bibliothek/files/admin.py
+++ bibliothek/files/admin.py
@@ -1,16 +1,29 @@
 # -*- coding: utf-8 -*-
+# Copyright (C) 2016-2017 Nathanael Philipp (jnphilipp) <mail@jnphilipp.org>
+#
+# This file is part of bibliothek.
+#
+# bibliothek is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# bibliothek is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with bibliothek.  If not, see <http://www.gnu.org/licenses/>.
 
 from django.contrib import admin
 from files.models import File
 
 
+@admin.register(File)
 class FileAdmin(admin.ModelAdmin):
-    list_display = ('id', 'file', 'updated_at')
-    search_fields = ('file',)
-
     fieldsets = [
         (None, {'fields': ['file', 'content_type', 'object_id']}),
     ]
-
-
-admin.site.register(File, FileAdmin)
+    list_display = ('id', 'file', 'updated_at')
+    search_fields = ('file',)
--- bibliothek/files/models.py
+++ bibliothek/files/models.py
@@ -1,4 +1,20 @@
 # -*- coding: utf-8 -*-
+# Copyright (C) 2016-2017 Nathanael Philipp (jnphilipp) <mail@jnphilipp.org>
+#
+# This file is part of bibliothek.
+#
+# bibliothek is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# bibliothek is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with bibliothek.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
 
@@ -9,14 +25,34 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class File(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Created at'))
-    updated_at = models.DateTimeField(auto_now=True, verbose_name=_('Updated at'))
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_('Created at')
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name=_('Updated at')
+    )
 
-    file = models.FileField(upload_to='files', max_length=4096, verbose_name=_('File'))
-
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, blank=True, null=True)
-    object_id = models.PositiveIntegerField(blank=True, null=True)
-    content_object = GenericForeignKey('content_type', 'object_id')
+    file = models.FileField(
+        upload_to='files',
+        max_length=4096,
+        verbose_name=_('File')
+    )
+    content_type = models.ForeignKey(
+        ContentType,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True
+    )
+    object_id = models.PositiveIntegerField(
+        blank=True,
+        null=True
+    )
+    content_object = GenericForeignKey(
+        'content_type',
+        'object_id'
+    )
 
     def to_json(self):
         return {'file_name': os.path.basename(self.file.name)}
