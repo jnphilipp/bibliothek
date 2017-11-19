40312aa526256925a6b81c6b25cc7b714a87a720
jnphilipp <mail@jnphilipp.org>
2017-11-19 17:27:17 +0100

    Update bibliothek app.

--- bibliothek/bibliothek/__init__.py
+++ bibliothek/bibliothek/__init__.py
@@ -1,3 +1,32 @@
 # -*- coding: utf-8 -*-
+# Copyright (C) 2017 Nathanael Philipp (jnphilipp) <mail@jnphilipp.org>
+#
+# This file is part of bibliothek.
+#
+# bibliothek is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# bibliothek is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with bibliothek.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Personal library tool."""
+
 
 default_app_config = 'bibliothek.apps.BibliothekConfig'
+
+__author__ = 'Nathanael Philipp'
+__copyright__ = 'Copyright 2017 Nathanael Philipp (jnphilipp)'
+__license__ = 'GPLv3'
+__maintainer__ = __author__
+__email__ = 'mail@jnphilipp.org'
+__app_name__ = 'bibliothek'
+__version_info__ = (0, 1, 0)
+__version__ = '.'.join(str(e) for e in __version_info__)
+__description__ = 'Personal library tool.'
