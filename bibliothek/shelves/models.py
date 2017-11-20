1cffb91528e80f4ab1d06ff299ffce2304f19483
jnphilipp <mail@jnphilipp.org>
2017-11-20 07:33:44 +0100

    Update shelves app.

--- bibliothek/shelves/models.py
+++ bibliothek/shelves/models.py
@@ -1,4 +1,20 @@
 # -*- coding: utf-8 -*-
+# Copyright (C) 2016-2017 Nathanael Philipp (jnphilipp) <mail@jnphilipp.org>
+#
+# This file is part of bibliothek.
+#
+# bibliothek is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# bibliothek is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with bibliothek.  If not, see <http://www.gnu.org/licenses/>.
 
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
@@ -7,15 +23,34 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class Acquisition(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Created at'))
-    updated_at = models.DateTimeField(auto_now=True, verbose_name=_('Updated at'))
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_('Created at')
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name=_('Updated at')
+    )
 
-    date = models.DateField(blank=True, null=True, verbose_name=_('Date'))
-    price = models.FloatField(default=0, verbose_name=_('Price'))
+    date = models.DateField(
+        blank=True,
+        null=True,
+        verbose_name=_('Date')
+    )
+    price = models.FloatField(
+        default=0,
+        verbose_name=_('Price')
+    )
 
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(
+        ContentType,
+        models.CASCADE
+    )
     object_id = models.PositiveIntegerField()
-    content_object = GenericForeignKey('content_type', 'object_id')
+    content_object = GenericForeignKey(
+        'content_type',
+        'object_id'
+    )
 
     def to_json(self):
         return {'date': str(self.date), 'price': str(self.price)}
@@ -30,15 +65,35 @@ class Acquisition(models.Model):
 
 
 class Read(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Created at'))
-    updated_at = models.DateTimeField(auto_now=True, verbose_name=_('Updated at'))
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_('Created at')
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name=_('Updated at')
+    )
 
-    started = models.DateField(blank=True, null=True, verbose_name=_('Date started'))
-    finished = models.DateField(blank=True, null=True, verbose_name=_('Date finished'))
+    started = models.DateField(
+        blank=True,
+        null=True,
+        verbose_name=_('Date started')
+    )
+    finished = models.DateField(
+        blank=True,
+        null=True,
+        verbose_name=_('Date finished')
+    )
 
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(
+        ContentType,
+        models.CASCADE
+    )
     object_id = models.PositiveIntegerField()
-    content_object = GenericForeignKey('content_type', 'object_id')
+    content_object = GenericForeignKey(
+        'content_type',
+        'object_id'
+    )
 
     def to_json(self):
         return {'started': str(self.started), 'finished': str(self.finished)}
--- bibliothek/shelves/templatetags/shelves.py
+++ bibliothek/shelves/templatetags/shelves.py
@@ -1,4 +1,20 @@
 # -*- coding: utf-8 -*-
+# Copyright (C) 2016-2017 Nathanael Philipp (jnphilipp) <mail@jnphilipp.org>
+#
+# This file is part of bibliothek.
+#
+# bibliothek is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# bibliothek is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with bibliothek.  If not, see <http://www.gnu.org/licenses/>.
 
 from django.conf import settings
 from django.template import Library
@@ -8,7 +24,10 @@ register = Library()
 
 @register.filter
 def currency_symbol(price):
-    return '%.02f %s' % (price, settings.CURRENCY_SYMBOL) if settings.CURRENCY_SYMBOL else price
+    if settings.CURRENCY_SYMBOL:
+        return '%.02f %s' % (price, settings.CURRENCY_SYMBOL)
+    else:
+        return '%.02f' % price
 
 @register.simple_tag
 def read_status(reads):
