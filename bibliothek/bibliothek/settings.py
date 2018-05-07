44ed00bf9cf52ad5a600d5d7ea38a8d7f389f75d
jnphilipp <mail@jnphilipp.org>
2018-05-07 14:14:03 +0200

    Replace django_bbostrap3 submodule with django_bootstrap4.

--- bibliothek/bibliothek/settings.py
+++ bibliothek/bibliothek/settings.py
@@ -77,7 +77,7 @@ INSTALLED_APPS = [
     'bibliothek',
     'bindings',
     'books',
-    'django_bootstrap3',
+    'django_bootstrap4',
     'files',
     'genres',
     'journals',
@@ -147,16 +147,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.' +
+                'UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.' +
+                'MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.' +
+                'CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.' +
+                'NumericPasswordValidator',
     },
 ]
 
