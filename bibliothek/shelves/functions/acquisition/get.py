d8a08391fcaa5933a9acc4854033ed0dbcb13814
J. Nathanael Philipp <nathanael@philipp.land>
2021-01-08 15:54:47 +0100

    Fix missing import in facquisition.get.

--- bibliothek/shelves/functions/acquisition/get.py
+++ bibliothek/shelves/functions/acquisition/get.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2016-2019 Nathanael Philipp (jnphilipp) <mail@jnphilipp.org>
+# Copyright (C) 2016-2021 J. Nathanael Philipp (jnphilipp) <nathanael@philipp.land>
 #
 # This file is part of bibliothek.
 #
@@ -16,12 +16,32 @@
 # You should have received a copy of the GNU General Public License
 # along with bibliothek.  If not, see <http://www.gnu.org/licenses/>.
 
+from books.models import Edition
+from django.db.models import Q
+from magazines.models import Issue
+from papers.models import Paper
 from shelves.models import Acquisition
+from typing import Optional
 
 from . import list as acquisition_list
 
 
-def by_pk(pk, edition=None, issue=None, paper=None):
+def by_pk(
+    pk: int,
+    edition: Optional[Edition] = None,
+    issue: Optional[Issue] = None,
+    paper: Optional[Paper] = None,
+) -> Optional[Acquisition]:
+    """Search for an acquisition by pk.
+
+    Args:
+        * pk: acquisition primary key
+        * edition: optional edition as base for acquisition to search through
+        * issue: optional issue as base for acquisition to search through
+        * paper: optional paper as base for acquisition to search through
+    Return:
+        * acquisition or None
+    """
     acquisitions = Acquisition.objects.all()
     if edition is not None:
         acquisitions = acquisitions.filter(editions=edition)
@@ -36,7 +56,14 @@ def by_pk(pk, edition=None, issue=None, paper=None):
         return None
 
 
-def by_term(term):
+def by_term(term: str) -> Optional[Acquisition]:
+    """Search for an acquisition by search term.
+
+    Args:
+        * term: search term
+    Return:
+        * acquisition or None
+    """
     acqus = acquisition_list.by_term(term)
 
     if acqus.count() == 0:
@@ -45,11 +72,14 @@ def by_term(term):
         if term.isdigit():
             acqus = acqus.filter(pk=term)
         else:
-            acqus = acqus.filter(Q(ni=term) | Q(jv=term) |
-                                 Q(editions__isbn=term) |
-                                 Q(editions__book__title=term) |
-                                 Q(papers__title=term) |
-                                 Q(editions__alternate_title=term))
+            acqus = acqus.filter(
+                Q(ni=term)
+                | Q(jv=term)
+                | Q(editions__isbn=term)
+                | Q(editions__book__title=term)
+                | Q(papers__title=term)
+                | Q(editions__alternate_title=term)
+            )
         if acqus.count() != 1:
             return None
     return acqus[0]
