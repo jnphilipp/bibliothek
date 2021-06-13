4981e414b20cd5003535835bdf73e558efc5d825
J. Nathanael Philipp <nathanael@philipp.land>
2021-06-13 22:44:12 +0200

    Update gnome search provider.

--- bibliothek/gnome-search-provider.py
+++ bibliothek/gnome-search-provider.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Copyright (C) 2016-2019 Nathanael Philipp (jnphilipp) <mail@jnphilipp.org>
+# Copyright (C) 2016-2021 Nathanael Philipp (jnphilipp) <mail@jnphilipp.org>
 #
 # This file is part of bibliothek.
 #
@@ -16,11 +16,14 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with bibliothek.  If not, see <http://www.gnu.org/licenses/>.
+"""Bibliothek GNOME search provider."""
 
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bibliothek.settings')
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bibliothek.settings")
 
 import django
+
 django.setup()
 
 import dbus
@@ -29,35 +32,40 @@ import dbus.service
 import os
 import sys
 
-from books.functions import edition as fedition
+from books.models import Edition
 from django.db.models import Q
 from gi.repository import GLib
-from magazines.functions import issue as fissue
-from papers.functions import paper as fpaper
+from magazines.models import Issue
+from papers.models import Paper
 
-search_bus_name = 'org.gnome.Shell.SearchProvider2'
+search_bus_name = "org.gnome.Shell.SearchProvider2"
 sbn = dict(dbus_interface=search_bus_name)
 
 
 class BibliothekSearchService(dbus.service.Object):
-    bus_name = 'org.gnome.bibliothek.SearchProvider'
-    _object_path = '/' + bus_name.replace('.', '/')
-    default_cover = os.path.abspath('./bibliothek/static/images/' +
-                                    'default_cover.jpg')
+    """Bibliothek search provider."""
+
+    bus_name = "org.gnome.bibliothek.SearchProvider"
+    _object_path = "/" + bus_name.replace(".", "/")
+    default_cover = os.path.abspath("./bibliothek/static/images/default_cover.jpg")
 
     def __init__(self):
+        """Init."""
         self.session_bus = dbus.SessionBus()
         bus_name = dbus.service.BusName(self.bus_name, bus=self.session_bus)
         dbus.service.Object.__init__(self, bus_name, self._object_path)
 
-    @dbus.service.method(in_signature='sasu', **sbn)
+    @dbus.service.method(in_signature="sasu", **sbn)
     def ActivateResult(self, id, terms, timestamp):
-        obj = self.get_obj(id)
+        """Activate result item."""
+        obj = self._get_obj(id)
         if obj is not None:
             if obj.files.count() > 1:
-                files = obj.files.filter(Q(file__iendswith='.epub') |
-                                         Q(file__iendswith='.pdf') |
-                                         Q(file__iendswith='.mobi'))
+                files = obj.files.filter(
+                    Q(file__iendswith=".epub")
+                    | Q(file__iendswith=".pdf")
+                    | Q(file__iendswith=".mobi")
+                )
                 if files.count() == 0:
                     path = obj.files.first().file.path
                 else:
@@ -65,87 +73,89 @@ class BibliothekSearchService(dbus.service.Object):
             else:
                 path = obj.files.first().file.path
 
-            if sys.platform == 'linux':
+            if sys.platform == "linux":
                 os.system('xdg-open "%s"' % path)
             else:
                 os.system('open "%s"' % path)
 
-    @dbus.service.method(in_signature='as', out_signature='as', **sbn)
+    @dbus.service.method(in_signature="as", out_signature="as", **sbn)
     def GetInitialResultSet(self, terms):
-        return self.get_result_set(terms)
+        """Get initial result set."""
+        return self._get_result_set(terms)
 
-    @dbus.service.method(in_signature='as', out_signature='aa{sv}', **sbn)
+    @dbus.service.method(in_signature="as", out_signature="aa{sv}", **sbn)
     def GetResultMetas(self, ids):
+        """Get result metas."""
         metas = []
         for id in ids:
-            obj = self.get_obj(id)
+            obj = self._get_obj(id)
             if obj is None:
                 continue
-            elif id.startswith('edition'):
-                name = f'{obj.book}'
-                gicon = obj.cover_image.path if obj.cover_image \
-                    else self.default_cover
-            elif id.startswith('paper'):
-                name = f'{obj}'
+            elif id.startswith("edition"):
+                name = f"{obj.book}"
+                gicon = obj.cover_image.path if obj.cover_image else self.default_cover
+            elif id.startswith("paper"):
+                name = f"{obj}"
                 gicon = self.default_cover
-            elif id.startswith('issue'):
-                name = f'{obj}'
-                gicon = obj.cover_image.path if obj.cover_image \
-                    else self.default_cover
+            elif id.startswith("issue"):
+                name = f"{obj}"
+                gicon = obj.cover_image.path if obj.cover_image else self.default_cover
 
-            metas.append({
-                'id': id,
-                'name': name,
-                'gicon': gicon
-            })
+            metas.append({"id": id, "name": name, "gicon": gicon})
         return metas
 
-    @dbus.service.method(in_signature='asas', out_signature='as', **sbn)
+    @dbus.service.method(in_signature="asas", out_signature="as", **sbn)
     def GetSubsearchResultSet(self, previous_results, new_terms):
-        return self.get_result_set(new_terms)
+        """Get subsearch result set."""
+        return self._get_result_set(new_terms)
 
-    @dbus.service.method(in_signature='asu', terms='as', timestamp='u', **sbn)
+    @dbus.service.method(in_signature="asu", terms="as", timestamp="u", **sbn)
     def LaunchSearch(self, terms, timestamp):
+        """Launch search."""
         pass
 
-    def get_obj(self, id):
-        if id.startswith('edition'):
-            return fedition.get.by_pk(id[8:])
-        elif id.startswith('paper'):
-            return fpaper.get.by_pk(id[6:])
-        elif id.startswith('issue'):
-            return fissue.get.by_pk(id[6:])
+    def _get_obj(self, id):
+        if id.startswith("edition"):
+            return Edition.get(id[8:])
+        elif id.startswith("paper"):
+            return Paper.get(id[6:])
+        elif id.startswith("issue"):
+            return Issue.get(id[6:])
         else:
             return None
 
-    def get_result_set(self, terms):
-        term = ' '.join(terms)
-        editions = fedition.list.by_term(term, has_file=True)
-        results = [f'edition-{e.pk}' for e in editions]
+    def _get_result_set(self, terms):
+        term = " ".join(terms)
+        editions = Edition.search(term, has_file=True)
+        results = [f"edition-{e.pk}" for e in editions]
 
-        papers = fpaper.list.by_term(term, has_file=True)
-        results += [f'paper-{p.pk}' for p in papers]
+        papers = Paper.search(term, has_file=True)
+        results += [f"paper-{p.pk}" for p in papers]
 
-        issues = fissue.list.by_term(term, has_file=True)
-        results += [f'issue-{i.pk}' for i in issues]
+        issues = Issue.search(term, has_file=True)
+        results += [f"issue-{i.pk}" for i in issues]
         return results
 
-    def notify(self, message, body='', error=False):
+    def notify(self, message, body="", error=False):
+        """Send notification."""
         try:
-            self.session_bus.get_object('org.freedesktop.Notifications',
-                                        '/org/freedesktop/Notifications'). \
-                Notify('bibliothek', 0, 'bibliothek', message, body, '',
-                       {'transient': False if error else True},
-                       0 if error else 3000,
-                       dbus_interface='org.freedesktop.Notifications')
+            self.session_bus.get_object(
+                "org.freedesktop.Notifications", "/org/freedesktop/Notifications"
+            ).Notify(
+                "bibliothek",
+                0,
+                "bibliothek",
+                message,
+                body,
+                "",
+                {"transient": False if error else True},
+                0 if error else 3000,
+                dbus_interface="org.freedesktop.Notifications",
+            )
         except dbus.DBusException as e:
-            print(f'Got error {e} while trying to display message {message}.')
+            print(f"Got error {e} while trying to display message {message}.")
 
 
-def main():
-    search_service = BibliothekSearchService()
+if __name__ == "__main__":
+    BibliothekSearchService()
     GLib.MainLoop().run()
-
-
-if __name__ == '__main__':
-    main()
